b07e84dd8b5cececca28479c7f17473f99eeb5b7
Wenfang Du <no_reply_mail@163.com>
2021-07-24 16:25:44 +0800

    added utils.js

---
+++ utils.js
@@ -0,0 +1,47 @@
+const remove = (...selectors) =>
+  selectors.forEach(selector => document.querySelectorAll(selector).forEach(el => el.remove()))
+
+const _focus = input => {
+  if (input) {
+    input.disabled &&= false
+
+    input.focus()
+
+    const end = input.value.length
+
+    input.setSelectionRange(end, end)
+  }
+}
+
+const focusOnSlash = (...selectors) =>
+  addEventListener('keyup', ({ key }) => {
+    if (key === '/' && !['input', 'textarea'].includes(document.activeElement.localName)) {
+      const input = selectors.map(selector => document.querySelector(selector)).find(Boolean)
+
+      _focus(input)
+    }
+  })
+
+const focusOnMountedAndOnSlash = (selector, { onMounted = true } = {}) => {
+  onMounted && _focus(document.querySelector(selector))
+
+  focusOnSlash(selector)
+}
+
+const disable = selector => (document.querySelector(selector).disabled ||= true)
+
+const warn = msg => {
+  document.title = msg
+
+  stop()
+
+  document.body.remove()
+
+  const observer = new MutationObserver(
+    ([{ target }]) => target.text === msg || (target.text = msg),
+  )
+
+  observer.observe(document.querySelector('title'), { childList: true })
+}
+
+const pause = ms => new Promise(resolve => setTimeout(resolve, ms))
