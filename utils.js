10e567b5d424a86197bc822b4813e10b95f3edf3
Wenfang Du <no_reply_mail@163.com>
2024-02-21 17:36:44 +0800

    chore: save

--- utils.js
+++ utils.js
@@ -31,22 +31,13 @@ const click = (selector, target = document) => {
   target.querySelector(selector)?.click()
 }
 
-const clickOnKey = (key, ...selectors) =>
-  addEventListener('keyup', evt => {
-    if (evt.key === key && !['input', 'textarea'].includes(document.activeElement.localName)) {
-      const els = selectors.map(selector => document.querySelector(selector))
-      if (els.some(Boolean)) {
-        blur()
-        els.forEach(el => el?.click())
-      }
+const clickOnEnter = selector => {
+  addEventListener('keyup', ({ key }) => {
+    if (key === 'Enter' && !['input', 'textarea'].includes(document.activeElement.localName)) {
+      click(selector)
     }
   })
-
-const clickOnEnter = (...selectors) => clickOnKey('Enter', ...selectors)
-
-const clickOnEsc = (...selectors) => clickOnKey('Escape', ...selectors)
-
-const clickOnSlash = (...selectors) => clickOnKey('/', ...selectors)
+}
 
 const warn = msg => {
   document.title = msg
